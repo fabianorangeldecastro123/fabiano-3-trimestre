37ddc5cb7e9867fd780cd0196b381d48282f3e6d
fabianorangeldecastro123 <108547559+fabianorangeldecastro123@users.noreply.github.com>
2024-11-18 15:11:24 -0300

    Add files via upload

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -21,14 +21,43 @@ async function quantidadeUsuarios() {
 
       const layout = {
         plot_bgcolor: getCSS('--bg-color'),
-        paper_bgcolor: getCSS('--bg-color')
-    }
-    
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+          text: 'Redes sociais com mais usuários no mundo',
+           x: 0,
+           font: {
+               color: getCSS('--primary-color'),
+               family: getCSS('--font'),
+               size: 30
+           }
+        }
+      // código omitido
 
-      const grafico = document.createElement('div')
+  },
+  xaxis: {
+      tickfont{
+          color: getCSS('-primary-color')
+      },
+      title: {
+          text: 'nome das redes sociais',
+          font: {
+              color: getCSS('--secondary-color')
+          }
+      }
+  },
+  yaxis: {
+          text: 'bilhões de usuários ativos',
+          font: {
+              color: getCSS('--secondary-color')
+          }
+      }
+  }
+
+// código omitido
+
+     const grafico = document.createElement('div')
       grafico.className = 'grafico'
       document.getElementById('graficos-container').appendChild(grafico)
       Plotly.newPlot(grafico, data, layout)
-      }
 
   quantidadesUsuarios()
